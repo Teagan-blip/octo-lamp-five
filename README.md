4250662b1838379a29ddd7bc713bb4409af6c71f
Teagan-blip <murray.teagan98@gmail.com>
2024-11-12 14:14:14 +1100

    almost out of time

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ The JavaScript code in index.js is unfinished. You will need to study it to figu
 
 Complete the following tasks:
 
-1. Calculate the score as the total of the number of correct answers
+DONE 1. Calculate the score as the total of the number of correct answers
 
-2. Add an Event listener for the submit button, which will display the score and highlight the correct answers when the button is clicked
+DONE 2. Add an Event listener for the submit button, which will display the score and highlight the correct answers when the button is clicked
 
-3. Add 2 more questions to the app (each question must have 4 options)
+DONE 3. Add 2 more questions to the app (each question must have 4 options) 
 
-4. Reload the page when the reset button is clicked (hint: search window.location)
+DONE 4. Reload the page when the reset button is clicked (hint: search window.location)
 
 5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 
-The assessment must be completed by 3:30pm. When you have finished, upload your work to GitHub Pages and share the link with your
+The assessment must be completed by 2:15pm. When you have finished, upload your work to GitHub Pages and share the link with your
 pod instructor by DM on Slack.
--- js/index.js
+++ js/index.js
@@ -16,15 +16,37 @@
 
       4. Reload the page when the reset button is clicked (hint: search window.location)
 
-      5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
-*************************** */
+      // 5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answerThe addEventListener() method of the EventTarget interface sets up a function that will be called whenever the specified event is delivered to the target.s
 
+
+      *************************** */
+
+
+  // The addEventListener() method of the EventTarget interface sets up a function that will be called whenever the specified event is delivered to the target.  
 window.addEventListener('DOMContentLoaded', () => {
   const start = document.querySelector('#start');
+  // event listener for clicking the start button
   start.addEventListener('click', function (e) {
     document.querySelector('#quizBlock').style.display = 'block';
     start.style.display = 'none';
+
+    // timer
+    if(!timer) {
+      timer = window.setInterval(function() { 
+        countdownTimer();
+      }, 1000); // every second
+    }
   });
+
+ 
+ 
+
+  
+  
+  
+  
+
+
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
@@ -43,6 +65,16 @@ window.addEventListener('DOMContentLoaded', () => {
       q: 'What is the capital of Australia',
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
+    },
+    {
+      q: 'What is name of the well know yellow pokemon?',
+      o: ['Pineapple', 'Pikachu', 'MellowYellow', 'Poke'],
+      a: 1,
+    },
+    {
+      q: 'What is the name of our nearest star?',
+      o: ['The big yellow ball', 'Star 007', 'The Sun', 'The Moon'],
+      a: 2,
     },
   ];
 
@@ -66,25 +98,79 @@ window.addEventListener('DOMContentLoaded', () => {
   // Calculate the score
   const calculateScore = () => {
     let score = 0;
+    let scoreDisplay = document.getElementById("score");
+    
+
+
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
+        
         let r = `radio_${index}_${i}`;
+        
         liElement = document.querySelector('#' + li);
         radioElement = document.querySelector('#' + r);
 
         if (quizItem.a == i) {
           //change background color of li element here
+          liElement.style.background = "green";
         }
+        
+        
 
         if (radioElement.checked) {
           // code for task 1 goes here
+          console.log(i);
+          console.log(quizItem["a"]);
+          console.log(r);
+          if (i == quizItem["a"]) {
+            score += 1;
+            console.log(score);
+            scoreDisplay.innerHTML = "Total Score: " + score;
+          }
+
+
         }
       }
     });
   };
+  const submit = document.getElementById("btnSubmit");
+  submit.addEventListener("click", calculateScore);
 
   // call the displayQuiz function
   displayQuiz();
 });
+
+const reset = document.getElementById("btnReset");
+function refresh(){
+  window.location.reload("Refresh")
+}
+reset.addEventListener("click", refresh);
+
+
+// Countdown timer section 
+let timer;
+let seconds = 5;
+
+function countdownTimer() {
+  if(seconds < 60) {
+    document.getElementById("time").innerHTML = seconds;
+  }
+  if (seconds >0 ) {
+     seconds--;
+  } else {
+     clearInterval(timer);
+    //  end the quiz, display the score and highlight correct answers
+    
+  }
+}
+
+document.getElementById("time") = function() {
+
+
+
+
+
+}
+document.getElementById("timer").innerHTML="1:00"; 
